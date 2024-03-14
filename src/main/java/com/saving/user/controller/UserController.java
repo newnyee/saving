93a9e259129c7a79022218f7dd365385d907b4ee
newnyee <mebbun0615@gmail.com>
2024-03-14 13:32:12 +0900

    refactor: UserController.reissueToken API 생성
    - 클라이언트에게 전달받은 Access 토큰의 userId와 refresh 토큰을 이용하여 새로운 토큰을 생성하는 API

--- src/main/java/com/saving/user/controller/UserController.java
+++ src/main/java/com/saving/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.saving.category.service.CategoryService;
 import com.saving.common.response.ApiResponse;
 import com.saving.common.response.TokenResponse;
 import com.saving.user.dto.LoginRequestDto;
+import com.saving.user.dto.ReissueTokenRequestDto;
 import com.saving.user.dto.UserCreateRequestDto;
 import com.saving.user.dto.UserCreatedResponseDto;
 import com.saving.user.service.UserService;
@@ -13,6 +14,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -39,11 +41,21 @@ public class UserController {
         return ApiResponse.created(user);
     }
 
-    @Operation(summary = "로그인", description = "사용자의 계정과 비밀번호를 입력하여 인증 후 Access Token, Refresh Token 발급이 완료됩니다.")
+    @Operation(summary = "로그인", description = "사용자의 계정과 비밀번호를 입력하여 인증 후 엑세스 토큰, 리프래쉬 토큰이 발급됩니다.")
     @PostMapping("/login")
     public ApiResponse<TokenResponse> login(
             @Valid @RequestBody LoginRequestDto loginRequestDto) {
 
         return ApiResponse.ok(userService.authenticationAndCreateToken(loginRequestDto));
+    }
+
+    @Operation(summary = "토큰 재발급", description = "새로운 엑세스 토큰, 리프래쉬 토큰이 발급됩니다.")
+    @PostMapping("/tokens")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<TokenResponse> reissueToken(
+            @RequestAttribute Long userId,
+            @Valid @RequestBody ReissueTokenRequestDto reissueTokenRequestDto) {
+
+        return ApiResponse.ok(userService.reissueToken(userId, reissueTokenRequestDto));
     }
 }
